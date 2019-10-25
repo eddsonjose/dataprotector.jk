03d4fcfb7611a35a8a82c349224451297823aaa4
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-24 21:22:03 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
 //DOM
 //by Eddson Jose
-let h1;
 //------------------------------------------------------------------
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  h1 = createElement('h1', 'DOM');
+  frameRate(10);
 }
 //------------------------------------------------------------------
 function draw() {
-  background(0);
-  h1.position(width/2, 0);
-  h1.style('font-size', '24px');
-  h1.style('color', color(random(150, 255)));
+  background(50);
+  noStroke();
+  fill(255);
+  circle(width/2, height/2, 30);
 }
 //------------------------------------------------------------------

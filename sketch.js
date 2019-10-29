d7746d681103ad88089741795e7cc770bf9f32ae
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-28 23:21:11 -0700

    update

--- sketch.js
+++ sketch.js
@@ -2,67 +2,103 @@
 //by Eddson Jose
 let counter = 0;
 let i = 0;
-let button;
-let input;
-let greeting;
 let showInfoBool = false;
+let popup = false;
+let button, input, title, greeting;
+let mic, vol, data;
 //-----------------------------------------------------------------------------
 let userInfo = [
-  "...gathering data...",
-  "...locating IP address...",
-  "...$_SERVER['REMOTE_ADDR'];...",
-  "...identification found...",
-  "...gathering banking data...",
-  "access granted."
+  "We support",
+  "Facebook",
+  "Twitter",
+  "Instagram",
+  "YouTube",
+  "Wells Fargo",
+  "Bank of America",
+  "Chase Bank"
 ];
 //-----------------------------------------------------------------------------
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(30);
   input = createInput();
-  input.position(width/2 - 100, height/2);
+  input.position(width/2 - 100, height*0.6);
   button = createButton('submit');
-  button.position(input.x + input.width, height/2);
+  button.position(input.x + input.width, height*0.6);
   button.mousePressed(response);
-  greeting = createElement('h1', 'what is your name?');
+  title = createElement('h1', 'DataProtector.jk');
+  title.style('color', '#ffffff');
+  title.style('width', '100%');
+  title.position(-10, height/3 + 40);
+  greeting = createElement('p', 'Secure logins for<br>your favorite websites.<br><br>Enter your email below.');
   greeting.style('color', '#ffffff');
   greeting.style('width', '100%');
-  greeting.position(0, height/2 - 60);
-
+  greeting.position(-10, height/3 + 90);
+  mic = new p5.AudioIn();
+  mic.start();
   textAlign(CENTER);
-  textSize(12);
+  textSize(30);
 }
 //-----------------------------------------------------------------------------
 function draw() {
   background(20, 70);
+  //Matrix & Slideshow Text Controller
   if (showInfoBool == true) {
     showInfo();
     showMatrix();
   } else {
     showInfoBool = false;
+    counter += 1;
+    if (counter == 50) {i++; counter = 0;}
+    if (i == 8) {i = 0;}
+    noStroke();
+    fill(255);
+    text(userInfo[i], width/2, height*0.7);
   }
-}
-//-----------------------------------------------------------------------------
-function showInfo() {
-  counter += 1;
-  if (counter == 250) {
-    i++; counter = 0;
-  }
-  noStroke();
+  //Volume
+  vol = mic.getLevel() * 100;
+  // console.log(vol);
   fill(255);
-  text(userInfo[i], width/2, height/2 + 50);
+  //Show showInfo()
+  if (vol >= 1 && showInfoBool == true) {
+    moreInfo();
+    noStroke();
+    fill(255);
+    textSize(12);
+    text("...data collected...", width/2, height*0.6);
+  }
+
+  // if (popup == true) {
+  //   doPopup();
+  // } else {
+  //   popup = false;
+  //   doPopup().hide;
+  // }
 }
-//-----------------------------------------------------------------------------
+//Hide Button & Input Field----------------------------------------------------
+function showInfo() {
+  button.hide();
+  input.hide();
+}
+//show 1s & 0s-----------------------------------------------------------------
 function showMatrix() {
+  stroke(255);
+  fill(255);
   text("1", random(0, width), random(0, height));
   text("0", random(0, width), random(0, height));
+}
+//show more 1s & 0s when user talks--------------------------------------------
+function moreInfo() {
+  stroke(255, 0, 0);
+  fill(255, 0, 0);
   text("1", random(0, width), random(0, height));
   text("0", random(0, width), random(0, height));
 }
 //-----------------------------------------------------------------------------
 function response() {
+  textSize(30);
   const name = input.value();
-  greeting.html('thank you ' + name + ' for your information');
+  greeting.html('Email sent to ' + name + '<br> Login to your favorite websites<br>from our email to keep<br>your data safe.');
   input.value('');
   showInfoBool = true;
 }
